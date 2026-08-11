e9b1add155955b60d08388d81a3a11dfa286e4da
LuancsDiniz <luancsdiniz@outlook.com>
2026-08-11 14:28:42 -0300

    chore: initialize project

---
+++ src/main/java/br/com/luan/diariorn/reporter/DiarioRnReporterApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.luan.diariorn.reporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioRnReporterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioRnReporterApiApplication.class, args);
+	}
+
+}
